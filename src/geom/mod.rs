5ca287ed621332d7b000137d832650b1073907eb
Phillip Raffnsøe <phillip@praffn.dk>
2020-11-03 09:40:41 +0100

    feat: add naïve shape list implementation

--- src/geom/mod.rs
+++ src/geom/mod.rs
@@ -10,6 +10,26 @@ pub trait Shape {
     fn hit(&self, ray: &Ray) -> Option<HitInfo>;
 }
 
+impl Shape for Vec<Sphere> {
+    fn hit(&self, ray: &Ray) -> Option<HitInfo> {
+        let mut hit: Option<HitInfo> = None;
+        for shape in self {
+            match shape.hit(ray) {
+                None => {}
+                Some(hit_a) => match &hit {
+                    None => hit = Some(hit_a),
+                    Some(hit_b) => {
+                        if hit_a.distance < hit_b.distance {
+                            hit = Some(hit_a)
+                        }
+                    }
+                },
+            }
+        }
+        hit
+    }
+}
+
 mod sphere;
 
 pub use sphere::Sphere;
